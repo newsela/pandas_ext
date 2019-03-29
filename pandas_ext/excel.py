db557365f9a68b324ea344be78e6cd676d6d6607
richiverse <richard.fernandez.nyc@gmail.com>
2019-03-29 08:36:54 -0400

    great suggestions luke

--- pandas_ext/excel.py
+++ pandas_ext/excel.py
@@ -1,9 +1,10 @@
-from os import path, remove
+"""Save xls[x] files to s3."""
+import os
+
 from tempfile import gettempdir
 
 import pandas as pd
 import s3fs
-
 
 from pandas_ext.common.utils import is_s3_path
 
@@ -14,15 +15,13 @@ def to_excel(df: pd.DataFrame, file_path: str, **kwargs) -> None:
         ext = file_path.split('.')[-1].lower()
         engine = dict(xls='xlwt', xlsx='xlsxwriter')[ext]
         path_removed = file_path.split('/')[-1]
-        tmp_file = path.join(gettempdir(), path_removed)
+        tmp_file = os.path.join(gettempdir(), path_removed)
         with pd.ExcelWriter(tmp_file, engine=engine) as writer:
-            print(tmp_file)
             df.to_excel(writer, **kwargs)
-        # writer.save()
 
         s3 = s3fs.S3FileSystem()
-        with s3.open(file_path, 'wb') as dest:
-            return dest.write(open(tmp_file, 'rb').read())
-        remove(tmp_file)
+        with open(tmp_file, 'rb') as source, s3.open(file_path, 'wb') as dest:
+            dest.write(source.read())
+        os.remove(tmp_file)
 
-    return df.to_excel(path, **kwargs)
+    return df.to_excel(file_path, **kwargs)
